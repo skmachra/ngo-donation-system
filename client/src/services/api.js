24d74cd0bcf634b0834767d4c7576d43ed1cb553
Sunil <sunilmachra2021@gmail.com>
2026-01-19 20:19:55 +0530

    feat: update API base URL to production environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ngo-donation-system-2yw8.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
